b4a7b67f2c9f36731f5a9298b3366e33607611ec
rominam <montiel.romina.emma@gmail.com>
2021-04-28 17:41:37 -0300

    component description y cajas box-info

---
+++ src/components/BoxInfo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+export default function boxInfo() {
+  return (
+    <section className="box-info">
+      <h3>Lorem ipsum dolor</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt.
+      </p>
+    </section>
+  );
+}
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './index.scss';
 import Header from '../../components/Header';
 import Banner from '../../components/Banner';
+import Description from '../../components/Description';
 
 export default function Home() {
   return (
     <section className="page-home">
       <Header />
       <Banner />
+      <Description />
     </section>
   );
 }
