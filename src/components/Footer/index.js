ccf76199d8d16a247c7e881a9550c09078b74fbc
rominam <montiel.romina.emma@gmail.com>
2021-04-30 16:42:53 -0300

    maquetado formulario y footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './index.scss';
+
+export default function footer() {
+  return (
+    <footer>
+      <div className="box-brand">
+        <img alt="" src="/img/kwiz.svg" />
+        <span>&copy;2021 kwiz</span>
+      </div>
+      <div className="nav">
+        <ul>
+          <li><a href="#description">Qué es</a></li>
+          <li><a href="#info-work">Cómo funciona</a></li>
+          <li><a href="#description">Contacto</a></li>
+        </ul>
+        <img alt="" src="/img/utopias.svg" />
+      </div>
+    </footer>
+  );
+}
